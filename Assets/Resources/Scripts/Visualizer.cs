91039cda49124d64982389be5f4585991ea07460
boosilguy <rjsgmlgggg@gmail.com>
2021-04-26 01:27:01 +0900

    Visual Effect 추가 개선 및 로고 추가
    Lerp로 마젠타까지 색상 변동 가능

--- Assets/Resources/Scripts/Visualizer.cs
+++ Assets/Resources/Scripts/Visualizer.cs
@@ -7,12 +7,14 @@ public class Visualizer : MonoBehaviour
 {
     public GameObject[] equalizer;
     public int intensity;
+    public int colorIntensity;
 
     WasapiAudioSource wasapiAudioSource;
-    
+    Color defaultEqualizerColor;
     void Awake()
     {
         wasapiAudioSource = this.GetComponent<WasapiAudioSource>();
+        defaultEqualizerColor = equalizer[0].GetComponent<Renderer>().material.GetColor("_Color");
     }
 
     void Update()
@@ -20,7 +22,10 @@ public class Visualizer : MonoBehaviour
         int idx = 10;
         foreach(GameObject gameObject in equalizer)
         {
-            gameObject.transform.localScale = new Vector3 (9.75f, wasapiAudioSource.GetSpectrumData()[idx] * 100 * intensity, 2f);
+            gameObject.transform.localScale = new Vector3 (9.75f, wasapiAudioSource.GetSpectrumData()[idx] * intensity, 2f);
+            
+            Material currentMaterial = gameObject.GetComponent<Renderer>().material;
+            currentMaterial.SetColor("_Color", Color.Lerp(defaultEqualizerColor, Color.magenta, wasapiAudioSource.GetSpectrumData()[idx] * colorIntensity));
 
             idx = idx + 4;
         }
